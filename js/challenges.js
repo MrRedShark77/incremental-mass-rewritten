22db5126786ffb6f5abf6b78b2edbe2c66730b7d
MrRedShark77 <mrredshark77@gmail.com>
2021-10-06 19:52:27 +0300

    v0.3 TS 10p1

--- js/challenges.js
+++ js/challenges.js
@@ -23,7 +23,7 @@ function updateChalHTML() {
     tmp.el.chal_desc_div.setDisplay(player.chal.choosed != 0)
     if (player.chal.choosed != 0) {
         let chal = CHALS[player.chal.choosed]
-        tmp.el.chal_ch_title.setTxt(`[${player.chal.choosed}]${player.chal.comps[player.chal.choosed].gte(75)?" Hardened":""} ${chal.title} [${player.chal.comps[player.chal.choosed]+"/"+chal.max} Completions]`)
+        tmp.el.chal_ch_title.setTxt(`[${player.chal.choosed}]${player.chal.comps[player.chal.choosed].gte(75)?" Hardened":""} ${chal.title} [${player.chal.comps[player.chal.choosed]+"/"+tmp.chal.max[player.chal.choosed]} Completions]`)
         tmp.el.chal_ch_desc.setHTML(chal.desc)
         tmp.el.chal_ch_reset.setTxt(CHALS.getReset(player.chal.choosed))
         tmp.el.chal_ch_goal.setTxt("Goal: "+CHALS.getFormat(player.chal.choosed)(tmp.chal.goal[player.chal.choosed])+CHALS.getResName(player.chal.choosed))
