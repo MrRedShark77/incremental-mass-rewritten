91cafa0d50b8e56ddec8a35e7c6b68f788618dd1
MrRedShark77 <mrredshark77@gmail.com>
2022-02-08 16:01:10 +0300

    v0.4.2.5p4

--- js/supernova/fermions.js
+++ js/supernova/fermions.js
@@ -297,13 +297,13 @@ function updateFermionsHTML() {
 
             tmp.el[id+"_div"].setDisplay(unl)
 
-            if (!unl) break
-
-            tmp.el[id+"_div"].setClasses({fermion_btn: true, [FERMIONS.names[i]]: true, choosed: tmp.fermions.ch[0] == i && tmp.fermions.ch[1] == x})
-            tmp.el[id+"_nextTier"].setTxt(fm(f.nextTierAt(player.supernova.fermions.tiers[i][x])))
-            tmp.el[id+"_tier_scale"].setTxt(getScalingName('fTier', i, x))
-            tmp.el[id+"_tier"].setTxt(format(player.supernova.fermions.tiers[i][x],0)+(tmp.fermions.maxTier[i][x] < Infinity?" / "+format(tmp.fermions.maxTier[i][x],0):""))
-            tmp.el[id+"_desc"].setHTML(f.desc(tmp.fermions.effs[i][x]))
+            if (unl) {
+                tmp.el[id+"_div"].setClasses({fermion_btn: true, [FERMIONS.names[i]]: true, choosed: tmp.fermions.ch[0] == i && tmp.fermions.ch[1] == x})
+                tmp.el[id+"_nextTier"].setTxt(fm(f.nextTierAt(player.supernova.fermions.tiers[i][x])))
+                tmp.el[id+"_tier_scale"].setTxt(getScalingName('fTier', i, x))
+                tmp.el[id+"_tier"].setTxt(format(player.supernova.fermions.tiers[i][x],0)+(tmp.fermions.maxTier[i][x] < Infinity?" / "+format(tmp.fermions.maxTier[i][x],0):""))
+                tmp.el[id+"_desc"].setHTML(f.desc(tmp.fermions.effs[i][x]))
+            }
         }
     }
 }
