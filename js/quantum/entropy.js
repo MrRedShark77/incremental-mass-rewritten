0cd40ff16fbd4326818165519f0d61e1fcbe02d2
MrRedShark77 <mrredshark77@gmail.com>
2023-06-03 17:29:12 +0300

    Patch 03.06.23 [3]

--- js/quantum/entropy.js
+++ js/quantum/entropy.js
@@ -24,7 +24,6 @@ const ENTROPY = {
         x = x.pow(exoticAEff(0,2))
 
         if (hasElement(185)) x = overflow(x,tmp.en.cap.max(10),0.25)
-        else x = x.min(tmp.en.cap)
 
         return x
     },
