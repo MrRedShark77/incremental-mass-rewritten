604c56e1a577f7df464f638353269ac43d74ae98
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2023-07-09 16:11:37 +0300

    Update ranks.js

--- js/ranks.js
+++ js/ranks.js
@@ -304,7 +304,7 @@ const PRESTIGES = {
         for (let i = 0; i < RANKS.names.length; i++) {
             let r = player.ranks[RANKS.names[i]]
             let br = E(tmp.beyond_ranks.max_tier)
-            if (hasBeyondPres(0,2)) x = x.add(br).mul(r.add(1))
+            if (hasBeyondPres(1,2)) x = x.add(br).mul(r.add(1))
             if (hasPrestige(0,18) && i == 0) r = r.mul(2)
            else x = x.mul(r.add(1))
         }
