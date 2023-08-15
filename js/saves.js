2076179f7cbbea105faefda2414f4af329b67064
MrRedShark77 <mrredshark77@gmail.com>
2023-08-15 22:02:35 +0300

    Update saves.js

--- js/saves.js
+++ js/saves.js
@@ -446,9 +446,9 @@ function cannotSave() { return tmp.supernova.reached && player.supernova.times.l
 function save(){
     let str = btoa(JSON.stringify(player))
     if (cannotSave() || findNaN(str, true)) return
-    if (localStorage.getItem("testSave") == '') wipe()
-    localStorage.setItem("testSave",str)
-    tmp.prevSave = localStorage.getItem("testSave")
+    if (localStorage.getItem("betaSave") == '') wipe()
+    localStorage.setItem("betaSave",str)
+    tmp.prevSave = localStorage.getItem("betaSave")
     if (tmp.saving < 1) {addNotify("Game Saved", 3); tmp.saving++}
 }
 
@@ -536,7 +536,7 @@ function importy() {
 }
 
 function loadGame(start=true, gotNaN=false) {
-    if (!gotNaN) tmp.prevSave = localStorage.getItem("testSave")
+    if (!gotNaN) tmp.prevSave = localStorage.getItem("betaSave")
     wipe()
     load(tmp.prevSave)
     
